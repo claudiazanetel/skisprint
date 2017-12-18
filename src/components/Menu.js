592bf9848c4e0fc4d07cc2cf970593875a6e5d76
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-12-18 19:15:11 +0100

    fixed menu's indentation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,40 +7,37 @@ class Menu extends Component {
   render() {
     return (
         <div className="container Menu">
-            <div>
-                <p>Menu</p>
-            </div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-                <img src={logo} alt={"logo"}></img>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    La gara
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a className="dropdown-item" href="#">Regolamento</a>
-                    <a className="dropdown-item" href="#">Percorso</a>
-                    <a className="dropdown-item" href="#">Viabilità</a>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Programma</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Staff</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Albo d'Oro</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt={"logo"}></img>
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                La gara
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Regolamento</a>
+                                <a className="dropdown-item" href="#">Percorso</a>
+                                <a className="dropdown-item" href="#">Viabilità</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Programma</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Staff</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Albo d'Oro</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
   }

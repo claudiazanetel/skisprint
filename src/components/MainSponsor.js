7ca4a4d571f77c2db59437a0efea68ce1d53b2b3
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-12-19 17:03:00 +0100

    fixed some bugs in css

--- src/components/MainSponsor.js
+++ src/components/MainSponsor.js
@@ -11,20 +11,20 @@ class MainSponsor extends Component {
     return (
       <div className="col-sm-12 sidebox">
         <div className="col-sm-12 mainSponsorDesktop">
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <h4>Main Sponsor</h4>
           </div>
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <a href="http://www.sanmartino.com/" target="_blank">
               <img  className="mainSponsor" src={apt} title="APT primiero" alt="APT primiero" />
             </a>
           </div>
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <a href="http://www.trentino.to/" target="_blank">
               <img className="mainSponsor" src={trentino} title="Trentino" alt="Trentino" />
             </a>
           </div>
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <a href="http://www.primieroenergia.com/" target="_blank"> 
               <img className="mainSponsor" src={primieroEnergia} title="Primiero Energia" alt="primiero energia" />
             </a>
--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -18,55 +18,53 @@ class Sponsors extends Component {
     return (
       <div className="col-xs-12 sponsorbox">
         <div className="col-xs-12">
-          <div className="col-xs-12">
-            <h4>Sponsors</h4>
+          <h4>Sponsors</h4>
+        </div>
+        <div className="row sponsorRow">
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="https://www.usprimiero.com/sponsor" target="_blank">
+              <img className="sponsor" src={poolPrimiero} title="US primiero" alt="US primiero" />
+            </a>
           </div>
-          <div className="row sponsorRow">
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="https://www.usprimiero.com/sponsor" target="_blank">
-                <img className="sponsor" src={poolPrimiero} title="US primiero" alt="US primiero" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.salomon.com/" target="_blank">
-                <img className="sponsor" src={salomon} title="Salomon" alt="Salomon" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.sportful.it" target="_blank">
-                <img className="sponsor" src={sportful} title="Sportful" alt="Sportful" />
-              </a>
-            </div>
-            <div className=" col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.primart.it/" target="_blank">
-                <img className="sponsor" src={artigiani} title="Consorzio Artigiani Imprenditori Primiero" alt="Consorzio Artigiani Imprenditori Primiero" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.caseificioprimiero.com/" target="_blank">
-                <img className="sponsor" src={caseificio} title="Caseificio Sociale" alt="Caseificio Sociale" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.crdolomiti.it/" target="_blank">
-                <img className="sponsor" src={casseruraliprimiero} title="Cassa Rurale Dolomiti" alt="Cassa Rurale Dolomiti" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.casserurali.it/" target="_blank">
-                <img className="sponsor" src={casserurali} title="Casse Rurali Trentine" alt="Casse Rurali Trentine" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.saittn.it/fc/" target="_blank">
-                <img className="sponsor" src={coop} title="Famiglia Cooperativa" alt="Famiglia Cooperativa" />
-              </a>
-            </div>
-            <div className="col-sm-4 col-xs-4 sponsorDiv">
-              <a href="http://www.escoprimiero.it/" target="_blank">
-                <img className="sponsor" src={esco} title="Esco Primiero" alt="Esco Primiero" />
-              </a>
-            </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.salomon.com/" target="_blank">
+              <img className="sponsor" src={salomon} title="Salomon" alt="Salomon" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.sportful.it" target="_blank">
+              <img className="sponsor" src={sportful} title="Sportful" alt="Sportful" />
+            </a>
+          </div>
+          <div className=" col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.primart.it/" target="_blank">
+              <img className="sponsor" src={artigiani} title="Consorzio Artigiani Imprenditori Primiero" alt="Consorzio Artigiani Imprenditori Primiero" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.caseificioprimiero.com/" target="_blank">
+              <img className="sponsor" src={caseificio} title="Caseificio Sociale" alt="Caseificio Sociale" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.crdolomiti.it/" target="_blank">
+              <img className="sponsor" src={casseruraliprimiero} title="Cassa Rurale Dolomiti" alt="Cassa Rurale Dolomiti" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.casserurali.it/" target="_blank">
+              <img className="sponsor" src={casserurali} title="Casse Rurali Trentine" alt="Casse Rurali Trentine" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.saittn.it/fc/" target="_blank">
+              <img className="sponsor" src={coop} title="Famiglia Cooperativa" alt="Famiglia Cooperativa" />
+            </a>
+          </div>
+          <div className="col-sm-4 col-xs-4 sponsorDiv">
+            <a href="http://www.escoprimiero.it/" target="_blank">
+              <img className="sponsor" src={esco} title="Esco Primiero" alt="Esco Primiero" />
+            </a>
           </div>
         </div>
       </div>

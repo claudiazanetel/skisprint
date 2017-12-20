c44ab00a15913406dae4902010e0f95664ebb1ec
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-12-20 11:25:24 +0100

    changed the program view

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
-import program2017 from '../assets/program/program2017.jpg';
-
 class Program extends Component {
 
   render() {
     return (
       <div className='col-sm-12 d-none d-md-block sidebox'>
-        <img className="program2017" src={program2017} alt={program2017}></img>
+        <h4>PROGRAMMA</h4>
+        <h5 className="time">14.00</h5>
+        <h6 className="race">Qualifiche Ski Sprint Young</h6>
+        <h5 className="time">15.00</h5>
+        <h6 className="race">Batterie Team Sprint</h6>
+        <h5 className="time">16.45</h5>
+        <h6 className="race">Finali Ski Sprint Young</h6>
+        <h5 className="time">17.30</h5>
+        <h6 className="race">Finali Team Sprint</h6>
       </div>
     );
   }

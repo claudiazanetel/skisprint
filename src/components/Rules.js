5aea9c38760ef944248c2a7d01de26fdbd5f4213
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-12-25 11:41:56 +0100

    deleted contacts in the rules components

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -18,15 +18,10 @@ class Rules extends Component {
           <div className="col-md-8 mainPage">
             <h1>REGOLAMENTI 2017</h1>
             <div className="row rules">
-              <div className="col-sm-6">
+              <div className="col-sm-12">
                 <h4>Download</h4>
                 <a href={regolamentoTeamSprint} download="regolamentoTeamSprint2017">Regolamento Team Sprint 2017</a><br></br>
                 <a href={regolamentoYoungSprint} download="regolamentoYoungSprint2017">Regolamento Ski Sprint Young 2017</a><br></br>
-              </div>
-              <div className="col-sm-6">
-                <h4>Contatti</h4>
-                <a href="http://www.usprimiero.com" target="_blank">usprimiero.com</a><br></br>
-                <a href="mailto:info@usprimiero.com?subject=Info-Skisprint">email</a>
               </div>
               <div className="col-sm-12 pdf d-none d-md-block">
                 <embed src={regolamentoTeamSprint} width="80%" height="375"/>

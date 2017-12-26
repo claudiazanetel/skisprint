ac00a42ec7d082a3f0e70577a3131f18e9382897
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-12-26 11:40:02 +0100

    added lista degli iscritti young  and deleted useless files

--- src/components/Iscritti.js
+++ src/components/Iscritti.js
@@ -7,7 +7,7 @@ import Patrocinio from "./Patrocinio";
 import Sponsors from "./Sponsors";
 
 // import iscrittiTeamSprint from '../files/regolamento2017.pdf';
-// import iscrittiYoungSprint from '../files/regolamento.young2017.pdf';
+import iscrittiYoungSprint from '../files/ordini_partenza_young.pdf';
 
 class Iscritti extends Component {
   render() {
@@ -17,14 +17,14 @@ class Iscritti extends Component {
         <div className="row">
           <div className="col-md-8 mainPage">
             <h1>LISTE ISCRITTI 2017</h1>
-            <p>Al momento non sono disponibili</p>
-{/*             <h4>Download</h4>
-            <a href={iscrittiTeamSprint} download="iscrittiTeamSprint">Lista Iscritti Team Sprint 2017</a><br></br>
-            <a href={iscrittiYoungSprint} download="iscrittiTeamSprint">Lista Iscritti Ski Sprint Young 2017</a><br></br>
+            {/* <p>Al momento non sono disponibili</p> */}
+            <h4>Download</h4>
+            {/* <a href={iscrittiTeamSprint} download="iscrittiTeamSprint">Lista Iscritti Team Sprint 2017</a><br></br> */}
+            <a href={iscrittiYoungSprint} target="_blank">Lista Iscritti Ski Sprint Young 2017</a><br></br>
             <div className="col-sm-12 pdf d-none d-md-block">
-              <embed src={iscrittiTeamSprint} width="80%" height="375"/>
+              {/* <embed src={iscrittiTeamSprint} width="80%" height="375"/> */}
               <embed src={iscrittiYoungSprint} width="80%" height="375"/>
-            </div> */}
+            </div>
           </div>
           <Sidebar />
           <Patrocinio />
